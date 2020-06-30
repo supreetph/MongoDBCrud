4b6b593d34b11e1b7c8187559ffb0cac35153413
Supreet <suprith.prakash.sp@gmail.com>
2020-06-30 11:22:48 +0530

    Add project files.

---
+++ MongoDBDemo/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDBDemo.Models;
+
+namespace MongoDBDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        IMongoClient mongoClient = new MongoClient("mongodb://localhost:27017");
+        // GET: ProductController
+        public ActionResult Index()
+        {
+            var db = mongoClient.GetDatabase("ecommerce");
+            var collection = db.GetCollection<Products>("products");
+            
+            var productsResult= collection.Find(new BsonDocument()).ToList();
+
+            return View(productsResult);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Products products)
+        {
+            try
+            {
+                var db = mongoClient.GetDatabase("ecommerce");
+                var collection = db.GetCollection<Products>("products");
+
+                collection.InsertOne(products);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MongoDBDemo/Models/Products.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoDBDemo.Models
+{
+    public class Products
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
+        public string item { get; set; }
+        public string qty { get; set; }
+    }
+}
